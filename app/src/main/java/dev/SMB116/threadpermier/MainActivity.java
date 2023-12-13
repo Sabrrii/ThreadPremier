5fb44d6ba9c4d6be497ab3260f8a1d0bc4ee4a13
Sabrrii <sabri.rabahia@sts-sio-caen.info>
2023-12-13 20:39:55 +0100

    version fonctionnel -> probleme grand nombre

--- app/src/main/java/dev/SMB116/threadpermier/MainActivity.java
+++ app/src/main/java/dev/SMB116/threadpermier/MainActivity.java
@@ -3,6 +3,7 @@ package dev.SMB116.threadpermier;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -19,46 +20,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText inputNumber;
-    private TextView tempsExecution;
-    private Button btnCalculer;
-    private ListView listePremier;
 
-    private NombrePremierRunnable nombrePremierRunnable;
-    private NombrePremier nombrePremier;
+    //Element de la vue
+    private EditText etInputNumber;
+    private TextView tvTempsExecution;
+    private Button btnCalculer;
+    private ListView lvListePremier;
+
+    private Button btnScroll;
+
+
+
+    //Element de la classe
+    private ArrayList<Integer>  listPremier;
+    private ArrayAdapter<Integer> adapter;
+    private float tempsExecution;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputNumber = findViewById(R.id.inputNumber);
-        tempsExecution = findViewById(R.id.tempsExecution);
+        //Initialisation des elements de la vue
+        etInputNumber = findViewById(R.id.inputNumber);
+        tvTempsExecution = findViewById(R.id.tempsExecution);
         btnCalculer = findViewById(R.id.btnCalculer);
-        listePremier = findViewById(R.id.listePremier);
+        lvListePremier = findViewById(R.id.listePremier);
+
+        btnScroll = findViewById(R.id.btnScroll);
+
+        //Initialisation des elements de la classe
+        listPremier = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listPremier);
+        lvListePremier.setAdapter(adapter);
+
+
 
         btnCalculer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculerNombrePremier();
+
+                listPremier.clear();
+
+                int nombreMax = Integer.parseInt(etInputNumber.getText().toString());
+                NombrePremierRunnable nombrePremierRunnable = new NombrePremierRunnable(listPremier, lvListePremier, tvTempsExecution, adapter);
+                nombrePremierRunnable.execute(nombreMax);
+
+
+
+            }//onClick
+
+
+        });//btnCalculer.setOnClickListener
+
+        btnScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lvListePremier.smoothScrollToPosition(listPremier.size());
             }
-        });
+        });//btnScroll.setOnClickListener
+
 
     }//onCreate
 
 
-    private void calculerNombrePremier(){
-        String  input = inputNumber.getText().toString();
-        if (input.isEmpty()){
-            inputNumber.setError("Veuillez entrer un nombre");
-            inputNumber.requestFocus();
-            return;
-        }
-        int nombreMax = Integer.parseInt(input);
-        NombrePremier nombrefini = nombrePremierRunnable.doInBackground(nombreMax);
-        tempsExecution.setText(String.valueOf(nombrefini.getTempsExecution()));
 
-    }//calculerNombrePremier
 
 
 }//MAIN
--- app/src/main/java/dev/SMB116/threadpermier/NombrePremierRunnable.java
+++ app/src/main/java/dev/SMB116/threadpermier/NombrePremierRunnable.java
@@ -1,39 +1,80 @@
 package dev.SMB116.threadpermier;
 
 import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NombrePremierRunnable extends AsyncTask<Integer, Integer, NombrePremier> {
-    private NombrePremier nombrePremier;
+public class NombrePremierRunnable extends AsyncTask<Integer, Integer, ArrayList<Integer>> {
+    private List<Integer> listePremier;
+    private ListView listeNbPremier;
+    private TextView tempsExecution;
+    private ArrayAdapter<Integer> adapter;
+
+    private  long tempDebut;
+
+    public NombrePremierRunnable(List<Integer> listePremier, ListView listeNbPremier, TextView tempsExecution, ArrayAdapter<Integer> adapter) {
+        this.listePremier = listePremier;
+        this.listeNbPremier = listeNbPremier;
+        this.tempsExecution = tempsExecution;
+        this.adapter = adapter;
+    }
+
+
 
     @Override
-    protected NombrePremier doInBackground(Integer... integers) {
+    protected void onPreExecute() {
+        super.onPreExecute();
+        tempDebut = System.currentTimeMillis();
+    }//onPreExecute
+
+    @Override
+    protected  ArrayList<Integer> doInBackground(Integer... integers) {
         int nombreMax = integers[0];
-        float tempsDebut = System.currentTimeMillis();
-
-        List<Integer> listePremier = new ArrayList<>();
-
+        ArrayList<Integer> listPremier = new ArrayList<>();
         for (int i = 1; i <= nombreMax; i++) {
-            if (nombrePremier.estPremier(i)){
-                listePremier.add(i);
-                updateProgress(i);
+            if (estPremier(i)){
+                listPremier.add(i);
+                publishProgress(i);
             }
         }
-        float tempsFin = System.currentTimeMillis()-tempsDebut;
-        return new NombrePremier(listePremier, tempsFin);
+        
+        return listPremier;
+
     }//doInBackground
 
 
-    protected  void updateProgress(Integer... values){
-        nombrePremier.updateListNombrePremier(values[0]);
-    }//publishProgress
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        listePremier.add(values[0]);
+        adapter.notifyDataSetChanged();
+    }//onProgressUpdate
+
+
+    @Override
+    protected void onPostExecute(ArrayList<Integer> integers) {
+        super.onPostExecute(integers);
+        long tempsFin = System.currentTimeMillis();
+        float temps = (tempsFin - tempDebut);
+        tempsExecution.setText(String.valueOf(temps));
+    }//onPostExecute
+
+    public boolean estPremier(int nombre){
+        for (int i = 2; i < nombre; i++) {
+            if (nombre % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }//estPremier
 
 
 
 
 
-
-}
+}//NombrePremierRunnable
